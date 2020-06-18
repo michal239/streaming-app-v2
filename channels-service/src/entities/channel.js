0fb6346460a7c586706dd7460f2bde3c6b543fbb
unknown <michalwot2019@gmail.com>
2020-06-18 19:17:05 +0200

    channel entity update

--- channels-service/src/entities/channel.js
+++ channels-service/src/entities/channel.js
@@ -1,15 +1,15 @@
 export default function buildMakeChannel({ generateID }) {
   return function makeChannel({
-    userId
+    userId,
+    streamKey,
+    views,
+    subscriptions
   }) {
     if (!userId) throw new Error('errorhere');
+    if (typeof views === "undefined" || typeof subscriptions === "undefined") throw new Error('error2');
 
-    const streamKey = generateID(12);
-    const views = 0;
-    const subscriptions = {
-      count: 0,
-      subscribers: []
-    }
+    if (!streamKey) streamKey = generateID(12);
+
     
     return Object.freeze({
       getUserId: () => userId,
--- channels-service/src/use-cases/add-channel.js
+++ channels-service/src/use-cases/add-channel.js
@@ -1,7 +1,15 @@
 export default function buildAddChannel({ makeChannel, channelsDb }) {
   return async function addChannel(userId) {
 
-    const channel = makeChannel({ userId });
+    const INITIAL_STATE = {
+      views: 0,
+      subscriptions: {
+        count: 0,
+        subscribers: []
+      }
+    }
+
+    const channel = makeChannel({ userId, ...INITIAL_STATE });
     
     return await channelsDb.insert({
       userId: channel.getUserId(),
