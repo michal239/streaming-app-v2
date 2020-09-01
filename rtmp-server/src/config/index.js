19ac200835a454b95f64368264bf2f8f60f9054e
unknown <michalwot2019@gmail.com>
2020-09-01 11:13:15 +0200

    rtmp cfg 2

---
+++ rtmp-server/src/config/index.js
@@ -0,0 +1,26 @@
+const rtmpDev = {
+  logType: 3,
+
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: 8888,
+    mediaroot: './mediaserver/media',
+    allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: './ffmpeg/bin/ffmpeg.exe',
+    tasks: [
+        {
+            app: 'live',
+            hls: true,
+            hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]'
+        }
+    ]
+  }
+}
