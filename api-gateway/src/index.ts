7c39392ea075efef71e165ab79e1e93e80b855e4
unknown <michalwot2019@gmail.com>
2020-09-24 19:35:29 +0200

    gateway usersclient

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello')
+require('module-alias/register')
+
+import UsersClient from './microservices/UsersService/UsersClient';
+
+async function main() {
+  try {
+    const res = await UsersClient.register({email:'dasd@o2.pl', password: 'dasd2d1asd', username: 'maciek'});
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main()
---
+++ api-gateway/src/microservices/UsersService/UsersClient.ts
@@ -0,0 +1,47 @@
+import grpc from 'grpc';
+import * as protoLoader from '@grpc/proto-loader';
+
+import { ILoginData, IRegisterData } from '@ts/interfaces/UsersClient.interface'
+
+const PROTO_PATH = __dirname + '../../../../../_proto/user.proto';
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+
+const userProto = grpc.loadPackageDefinition(packageDefinition).user;
+
+const UsersClient = new userProto.UserService('localhost:3000', grpc.credentials.createInsecure());
+
+function login(data: ILoginData) {
+  return new Promise((resolve, reject) => {
+    UsersClient.login(data, (err: any, res: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    })
+  })
+}
+
+function register(data: IRegisterData) {
+  return new Promise((resolve, reject) => {
+    UsersClient.register(data, (err: any, res: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    })
+  })
+}
+
+export default Object.freeze({
+  login,
+  register
+});
