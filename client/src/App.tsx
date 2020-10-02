1667315165a2ad616260d9d4a86cccc78f3affee
unknown <michalwot2019@gmail.com>
2020-10-02 03:15:52 +0200

    sass setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Homepage from './pages/Homepage/Homepage';
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <>
+      <Router>
+        <Route path="/" exact component={Homepage} />
+      </Router>
+    </>
   )
 }
 
---
+++ client/src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <h2>Hello</h2>
+  );
+}
+
+export default Homepage;
